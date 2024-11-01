4a4c989d5ce06efbc974a22c1b608836e3f34907
Tajammal Waheed <44042193+tajammalw@users.noreply.github.com>
2024-11-01 22:10:55 +0000

    Created store variables, and tested them.

---
+++ index.js
@@ -0,0 +1,52 @@
+//Creating the eCommerce Store Variables.
+
+// Store Name
+const storeName = "Golden-Crust";
+
+// Store Type
+const storeType = "Bakery";
+
+// Store Products 
+const product1 = "Breads";
+const product2 = "Pasteries";
+const product3 = "Cakes & Cups";
+const product4 = "Cookies";
+const product6 = "Pies & Tarts";
+const product7 = "Speciality Items";
+const product8 = "Beverages";
+
+//Product Prices
+const price1 = 19.99;
+const price2 = 24.99;
+const price3 = 39.99;
+const price4 = 19.99;
+const price6 = 19.99;
+const price7 = 24.99;
+const price8 = 7.99;
+
+// Store Location
+const storeLocation = "2115 Wonderland Street, Dream Blvd, Phoenix AZ 34576";
+
+// Store Hours
+const storeHours = "Monday - Friday: 9 AM - 6 PM, Saturday: 10 AM - 4 PM, Sunday: Closed";
+
+// Testing the variables
+console.log(`The store name is ${storeName}, and the store type is ${storeType}.`);
+console.log(`The productName is ${product1}, and it's price is ${price1}.`);
+console.log(`The productName is ${product2}, and it's price is ${price2}.`);
+console.log(`The productName is ${product3}, and it's price is ${price3}.`);
+console.log(`The productName is ${product4}, and it's price is ${price4}.`);
+console.log(`The productName is ${product6}, and it's price is ${price6}.`);
+console.log(`The productName is ${product7}, and it's price is ${price7}.`);
+console.log(`The store location is ${storeLocation}.`);
+console.log(`The store hours are ${storeHours}.`);
+
+
+
+
+
+
+
+
+    
+
