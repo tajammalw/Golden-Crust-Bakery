c4b6600bbb882cf1ac7de46e5805f9347e4456cc
Tajammal Waheed <44042193+tajammalw@users.noreply.github.com>
2024-11-24 17:17:59 +0000

    // Added Function to display products and prices.

--- index.js
+++ index.js
@@ -49,184 +49,59 @@ const productCategory8 = "Beverages";
 const beverages = ["Coffee & Espresso", "Tea", "Hot Chocolate"];
 const beveragesPrices = ["$3.00", "$2.50", "$3.00"];
 
-
-// Displaying the Products and Prices
-console.log(`\nThe product category is ${productCategory1}:`);
-console.log(`The productName is ${product1}, and it's price is ${price1}.`);
-console.log(`The productName is ${product2}, and it's price is ${price2}.`);
-console.log(`The productName is ${product3}, and it's price is ${price3}.`);
-console.log(`The productName is ${product4}, and it's price is ${price4}.`);
-console.log(`The productName is ${product5}, and it's price is ${price5}.`);
-
-console.log(`\nThe product category is ${productCategory2}:`);
-console.log(`The productName is ${product6}, and it's price is ${price6}.`);
-console.log(`The productName is ${product7}, and it's price is ${price7}.`);
-console.log(`The productName is ${product8}, and it's price is ${price8}.`);
-console.log(`The productName is ${product9}, and it's price is ${price9}.`);
-console.log(`The productName is ${product10}, and it's price is ${price10}.`);
-
-console.log(`\nThe product category is ${productCategory3}:`);
-console.log(`The productName is ${product11}, and it's price is ${price11}.`);
-console.log(`The productName is ${product12}, and it's price is ${price12}.`);
-console.log(`The productName is ${product13}, and it's price is ${price13}.`);
-
-console.log(`\nThe product category is ${productCategory4}:`);
-console.log(`The productName is ${product14}, and it's price is ${price14}.`);
-console.log(`The productName is ${product15}, and it's price is ${price15}.`);
-console.log(`The productName is ${product16}, and it's price is ${price16}.`);
-console.log(`The productName is ${product17}, and it's price is ${price17}.`);
-console.log(`The productName is ${product18}, and it's price is ${price18}.`);
-
-console.log(`\nThe product category is ${productCategory5}:`);
-console.log(`The productName is ${product19}, and it's price is ${price19}.`);
-console.log(`The productName is ${product20}, and it's price is ${price20}.`);
-console.log(`The productName is ${product21}, and it's price is ${price21}.`);
-console.log(`The productName is ${product22}, and it's price is ${price22}.`);
-
-console.log(`\nThe product category is ${productCategory6}:`);
-console.log(`The productName is ${product23}, and it's price is ${price23}.`);
-console.log(`The productName is ${product24}, and it's price is ${price24}.`);
-console.log(`The productName is ${product25}, and it's price is ${price25}.`);
-
-console.log(`\nThe product category is ${productCategory7}:`);
-console.log(`The productName is ${product26}, and it's price is ${price26}.`);
-console.log(`The productName is ${product27}, and it's price is ${price27}.`);
-console.log(`The productName is ${product28}, and it's price is ${price28}.`);
-console.log(`The productName is ${product29}, and it's price is ${price29}.`);
-
-console.log(`\nThe product category is ${productCategory8}:`);
-console.log(`The productName is ${product30}, and it's price is ${price30}.`);
-console.log(`The productName is ${product31}, and it's price is ${price31}.`);
-console.log(`The productName is ${product32}, and it's price is ${price32}.`);
-console.log(`\nThe store location is ${storeLocation}.`);
-console.log(`\nThe store hours are ${storeHours}.`); 
-
-
-//aisd-jse-on-the-job-2
-console.log(`\naisd-jse-on-the-job-2`)
-console.log(`\nWelcome to ${storeName}. We are the best ${storeType} in town.`);
-console.log(`\nHere are the products we currently offer:`);
-// Building arrays from the product and price variables:
-let breads = ["Artisan Loaves", "Baguettes", "Ciabatta", "Focaccia", "Bagels"];
-let breadsPrices = ["$5.00", "$3.00", "$4.00", "$4.50", "$1.50"];
-let pastries = ["Croissants", "Danishes", "Scones", "Cinnamon Rolls", "Turnovers"];
-let pastriesPrices = ["$3.00", "$3.50", "$2.75", "$3.25", "$3.00"];
-let cakesCupcakes = ["Classic Cakes", "Cupcakes", "Cheesecakes"];
-let cakesCupcakesPrices = ["$25.00", "$2.50", "$30.00"];
-let cookies = ["Chocolate Chip Cookies", "Oatmeal Raisin Cookies", "Sugar Cookies", "Macarons", "Shortbread"];
-let cookiesPrices = ["$1.50", "$1.50", "$1.50", "$1.50", "$2.00"];
-let savoryItems = ["Quiches", "Savory Scones", "Puff Pastry Bites", "Sandwiches"];
-let savoryItemsPrices = ["$4.00", "$2.75", "$3.50", "$7.00"];
-let piesTarts = ["Fruit Pies", "Cream Pies", "Tarts"];
-let piesTartsPrices = ["$15.00", "$16.00", "$14.00"];
-let specialityItems = ["Eclairs", "Muffins", "Brioche", "Gluten-Free or Vegan Options"];
-let specialityItemsPrices = ["$3.00", "$2.75", "$3.25", "$5.00"];
-let beverages = ["Coffee & Espresso", "Tea", "Hot Chocolate"];
-let beveragesPrices = ["$3.00", "$2.50", "$3.00"];
-
-
-// Display the products & the Prices Lists:
-console.log(`\n${productCategory1}:
-    ${breads[0]} - ${breadsPrices[0]}  
-    ${breads[1]} - ${breadsPrices[1]}
-    ${breads[2]} - ${breadsPrices[2]}
-    ${breads[3]} - ${breadsPrices[3]}
-    ${breads[4]} - ${breadsPrices[4]}`);
-
-console.log(`\n${productCategory2}:
-    ${pastries[0]} - ${pastriesPrices[0]}
-    ${pastries[1]} - ${pastriesPrices[1]}
-    ${pastries[2]} - ${pastriesPrices[2]}
-    ${pastries[3]} - ${pastriesPrices[3]}
-    ${pastries[4]} - ${pastriesPrices[4]}`);
-
-console.log(`\n${productCategory3}:
-    ${cakesCupcakes[0]} - ${cakesCupcakesPrices[0]}
-    ${cakesCupcakes[1]} - ${cakesCupcakesPrices[1]}
-    ${cakesCupcakes[2]} - ${cakesCupcakesPrices[2]}`);
-
-console.log(`\n${productCategory4}:
-    ${cookies[0]} - ${cookiesPrices[0]}
-    ${cookies[1]} - ${cookiesPrices[1]}
-    ${cookies[2]} - ${cookiesPrices[2]}
-    ${cookies[3]} - ${cookiesPrices[3]}
-    ${cookies[4]} - ${cookiesPrices[4]}`);
-
-console.log(`\n${productCategory5}:
-    ${savoryItems[0]} - ${savoryItemsPrices[0]}
-    ${savoryItems[1]} - ${savoryItemsPrices[1]}
-    ${savoryItems[2]} - ${savoryItemsPrices[2]}
-    ${savoryItems[3]} - ${savoryItemsPrices[3]}`);
-
-console.log(`\n${productCategory6}:
-    ${piesTarts[0]} - ${piesTartsPrices[0]}
-    ${piesTarts[1]} - ${piesTartsPrices[1]}
-    ${piesTarts[2]} - ${piesTartsPrices[2]}`);
-
-console.log(`\n${productCategory7}:
-    ${specialityItems[0]} - ${specialityItemsPrices[0]}
-    ${specialityItems[1]} - ${specialityItemsPrices[1]}
-    ${specialityItems[2]} - ${specialityItemsPrices[2]}
-    ${specialityItems[3]} - ${specialityItemsPrices[3]}`);
-
-console.log(`\n${productCategory8}:
-    ${beverages[0]} - ${beveragesPrices[0]}
-    ${beverages[1]} - ${beveragesPrices[1]}
-    ${beverages[2]} - ${beveragesPrices[2]}`);
-
-// Adding new products to Beverages
-beverages.push("Lemonade", "Iced Matcha Latte", "Chai Latte");
-beveragesPrices.push("$2.50", "$4.00", "$3.50");
-console.log(`\nWe have added new products to ${productCategory8}:`)
-
-// Displaying the updated Beverages list
-console.log(`\nUpdated ${productCategory8} List:
-    ${beverages[0]} - ${beveragesPrices[0]}
-    ${beverages[1]} - ${beveragesPrices[1]}
-    ${beverages[2]} - ${beveragesPrices[2]}
-    ${beverages[3]} - ${beveragesPrices[3]}
-    ${beverages[4]} - ${beveragesPrices[4]}
-    ${beverages[5]} - ${beveragesPrices[5]}
-    ${beverages[6]} - ${beveragesPrices[6]}`);
-
-// Removing last item from Cookies
-cookies.pop();
-cookiesPrices.pop();
-console.log(`\nUnfortunately, Shortbread in ${productCategory4} is out-of-stock.`);
-// Displaying the updated Cookies list
-console.log(`\nUpdated ${productCategory4} List:
-    ${cookies[0]} - ${cookiesPrices[0]}
-    ${cookies[1]} - ${cookiesPrices[1]}
-    ${cookies[2]} - ${cookiesPrices[2]}
-    ${cookies[3]} - ${cookiesPrices[3]}`);
+// Function to display productCategory1
+const displayBreadsAndPrices = (breads, prices) => {
     
-// Example of slice: Showing a selection of featured products
-let featuredProducts = piesTarts.slice(0, 3);
-console.log(`\nThis week our featured products are:, ${featuredProducts}`);
+    breads.forEach((bread, index) => {
+        logMessage(`${bread}: ${prices[index]}`);
+    });
+};
 
-// Example of concat: Add new products from a shipment to the store's inventory
-let newBreadsItems = ["Brioche Loaf", "Garlic Herb Bread"];
-let newBreadsPrices = [5.95, 5.75];
-breads = breads.concat(newBreadsItems);
-breadsPrices = breadsPrices.concat(newBreadsPrices);
+// Function to display productCategory2
+const displayPastriesAndPrices = (pastries, prices) => {
+    pastries.forEach((pastry, index) => {
+        logMessage(`${pastry}: ${prices[index]}`);
+    });
+}
 
-console.log(`\nWe have added new breads to our inventory:`);
-console.log(`Breads:
-    ${breads[0]} - $${breadsPrices[0]}
-    ${breads[1]} - $${breadsPrices[1]}
-    ${breads[2]} - $${breadsPrices[2]}
-    ${breads[3]} - $${breadsPrices[3]}
-    ${breads[4]} - $${breadsPrices[4]}
-    ${breads[5]} - $${breadsPrices[5]}
-    ${breads[6]} - $${breadsPrices[6]}`);
+// Function to display productCategory3
+const displayCakesCupcakesAndPrices = (cakesCupcakes, prices) => {
+    cakesCupcakes.forEach((cake, index) => {
+        logMessage(`${cake}: ${prices[index]}`);
+    });
+}
 
-    console.log(`\nThanks for visiting ${storeName}`);
-    console.log(`\nHere are our store hours for reference: ${storeHours}`);    
+//Function to display productCategory4
+const displayCookiesAndPrices = (cookies, prices) => {
+    cookies.forEach((cookie, index) => {
+        logMessage(`${cookie}: ${prices[index]}`);
+    });
+};
 
+//Function to display productCategory5
+const displaySavoryItemsAndPrices = (savoryItems, prices) => {
+    savoryItems.forEach((item, index) => {
+        logMessage(`${item}: ${prices[index]}`);
+    });
+};
 
+//Function to display productCategory6
+const displayPiesTartsAndPrices = (piesTarts, prices) => {
+    piesTarts.forEach((pie, index) => {
+        logMessage(`${pie}: ${prices[index]}`);
+    });
+};
 
+//Function to display productCategory7
+const displaySpecialityItemsAndPrices = (specialityItems, prices) => {
+    specialityItems.forEach((item, index) => {
+        logMessage(`${item}: ${prices[index]}`);
+    });
+};
 
-
-
-    
-
+//Function to display productCategory8
+const displayBeveragesAndPrices = (beverages, prices) => {
+    beverages.forEach((beverage, index) => {
+        logMessage(`${beverage}: ${prices[index]}`);
+    });
+};
